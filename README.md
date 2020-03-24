32a010db91eefd87222410f266abc72391143038
Mya-Mya <interleukin413@gmail.com>
2020-03-24 16:33:08 +0900

    CUIへ変更

--- README.md
+++ README.md
@@ -4,6 +4,3 @@
 * opencv-python 4.2.0.32
 * tensorflow 1.4.0
 * Pillow 7.0.0
-* tkinter
-## 注釈
-* tkinterはpipでインストールしない。
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import cmd
+
+
+class BeautyInlet2CUI(cmd.Cmd):
+    intro = '''BeautyInlet2 - ヌシを求めコーヒーを捨てる -
+    Mya-Mya(C)2020'''
+    prompt = 'BI2'
+    doc_header = 'Help on BI2'
